78edf9fe96fd764d8666d1906768ec3445b546a8
Nathan Bridgewater <nathan@iws.io>
2023-10-14 17:00:53 -0500

    Fixing deps.

--- demo/src/index.js
+++ demo/src/index.js
@@ -1,4 +1,4 @@
-
+import { StrictMode } from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
@@ -6,9 +6,9 @@ import './index.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
-	<React.StrictMode>
+	<StrictMode>
 		<App />
-	</React.StrictMode>
+	</StrictMode>
 )
 
 // If you want to start measuring performance in your app, pass a function
